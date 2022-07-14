a75961fc2d3036bf6c743ed650f26c4e5d3433df
fabienr <fabienr@loisirsencheres.com>
2022-07-14 13:51:54 +0200

    ignore dist folder content

--- dist/services/DataService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dataRouter = (0, express_1.Router)();
-dataRouter.get('/', (req, res) => {
-    res.send({ "message": 'Welcome to data part of this API' });
-});
-exports.default = dataRouter;
